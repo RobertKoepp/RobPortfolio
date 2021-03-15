fdf7bad7aa65ecb0db97c3f5f36ae95d478772db
Robert Koepp <rkoepp2@gmail.com>
2021-03-14 23:59:17 -0700

    footer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import AboutMe from "./components/AboutMe";
 import Services from "./components/Services";
 import Portfolio from "./components/Portfolio";
 import Contacts from "./components/Contacts";
+import Footer from "./components/Footer";
 
 
 function App() {
@@ -40,6 +41,7 @@ function App() {
     <Services />
     <Portfolio />
     <Contacts />
+    <Footer />
     </>
   );
 }
